94f1710db27d7b1dc76f06c156ecc4e847f737aa
Ryan Ovrevik <rovrevik@ovrevik.com>
2013-10-26 14:16:52 -0400

    added some documentation about ruby and puppet versions

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,5 +1,21 @@
+# What ruby version is executing on the guest?
+# for the "official" vagrant boxes: cat /home/vagrant/postinstall.sh | grep ruby_home
+# for Puppet Labs boxes: cat /home/vagrant/ruby.sh | grep RUBY_VERSION=
+
+# What puppet version is executing on the guest?
+# The Puppet Labs and Vagrant boxes install puppet with gem. Interesting,
+# given that puppet says that installing from gems is "Not Recommended". I
+# wonder why? What is the trade off? Is it just more complicated for the user?
+# The vagrant box is backed in. (currently 2.7.19)
+# The Puppet Labs boxes simply install the latest puppet vi gem. (currently 3.1.1)
+# executed: gem list | grep puppet
+# or: sudo find / -name  puppet
+
 # Where are the puppet logs on the guest?
 # /var/lib/puppet/reports/ and /var/lib/puppet/state/state.yaml
+
+# How does vagrant start puppet apply on the guest?
+# TODO
 
 exec { "apt-get update":
   path => "/usr/bin",
