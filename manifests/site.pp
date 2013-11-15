8f8db715417f4d666bb305d18dc71b3ef5c44950
Ryan Ovrevik <rovrevik@ovrevik.com>
2013-11-15 17:39:27 -0500

    Added reasoning for not using native augeas packages.

--- manifests/site.pp
+++ manifests/site.pp
@@ -31,6 +31,13 @@ exec { "apt-get update":
 # http://docs.puppetlabs.com/references/latest/type.html#augeas
 # http://www.augeas.net/
 
+# What augeas packages are available?
+# apt-cache search augeas
+# dpkg --get-selections | grep augeas
+
+# Go ahead and install the augeas command line tools in addition to the libraries that are needed for puppet.
+# installing the "libaugeas-ruby", "libaugeas-ruby1.8" packages seemed like a good idea but installed files were not
+# readily accessible to the ruby version that is used to execute puppet.
 $augeas_packages=["augeas-tools", "libaugeas-dev", "pkg-config"]
 package { $augeas_packages:
   ensure  => present,
@@ -39,7 +46,6 @@ package { $augeas_packages:
 
 # Where do the ruby bindings for augeas get installed?
 # See that stuff is installed into /usr/lib/ruby/1.8/i686-linux/_augeas.so and not in /opt/vagrant_ruby
-# dpkg --get-selections | grep augeas
 # dpkg-query -L libaugeas-ruby1.8
 
 # Add augeas support via gem instead for the native package manager. 
