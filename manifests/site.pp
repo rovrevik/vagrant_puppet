2cb21e11d918381bb9cde2eb0c62f76e06ef8006
Ryan Ovrevik <rovrevik@ovrevik.com>
2013-11-19 12:49:41 -0500

    Added documentation about what needed to happen for augeas to function correctly.

--- manifests/site.pp
+++ manifests/site.pp
@@ -27,8 +27,9 @@ exec { "apt-get update":
   path => "/usr/bin",
 }
 
-# Just add a comment to any old shellvars file. Fails with Error: Could not find a suitable provider for augeas.
-# This does not work because the bindings are not visible to the ruby used to execute puppet on the guest.
+# Just add a comment to any old file. Fails with Error: Could not find a suitable provider for augeas.
+# This did not work initially because the bindings were not visible to the ruby used to execute puppet on the guest.
+# This works now because the bindings installed with the ruby-augeas gem are visible in successive manifests executed after the gem is installed.
 augeas { "hosts_11_15_2013":
   context => "/files/etc/hosts",
   onlyif => "match #comment[. = 'change hosts_11_15_2013'] size == 0",
